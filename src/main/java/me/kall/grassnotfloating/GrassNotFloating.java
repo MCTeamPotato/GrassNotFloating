713abcf29583fa3f8892a7db4d56c7a2b7f7ff23
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-11-25 19:17:00 +0800

    cleaner classes

--- src/main/java/me/kall/grassnotfloating/GrassNotFloating.java
+++ src/main/java/me/kall/grassnotfloating/GrassNotFloating.java
@@ -1,6 +1,5 @@
 package me.kall.grassnotfloating;
 
-import com.google.common.base.Suppliers;
 import me.kall.duplicationless.data.ChunkData;
 import me.kall.duplicationless.event.BlockChangeEvent;
 import me.kall.duplicationless.util.Executor;
@@ -31,7 +30,7 @@ public final class GrassNotFloating {
     public static final String MOD_ID = "grassnotfloating";
     public static final Logger LOGGER = LogManager.getLogger(GrassNotFloating.class);
 
-    private static final Supplier<BlockState> AIR = Suppliers.memoize(Blocks.AIR::defaultBlockState);
+    private static final Supplier<BlockState> AIR = Blocks.AIR::defaultBlockState;
 
     public GrassNotFloating(@NotNull FMLJavaModLoadingContext context) {
         IEventBus modBus = context.getModEventBus();
@@ -47,12 +46,11 @@ public final class GrassNotFloating {
     }
 
     private void blockChange(@NotNull BlockChangeEvent event) {
-        if (((Trackable)event.oldState().getBlock()).float$tracked()) {
-            ServerLevel level = event.level();
-            long chunk = event.chunkPos();
-            long block = event.blockPos();
-            level.getServer().execute(() -> Unfloatable.get(level).add(chunk, block));
-        }
+        ServerLevel level = event.level();
+        long chunk = event.chunkPos();
+        long block = event.blockPos();
+        if (((Trackable)event.oldState().getBlock()).float$tracked()) level.getServer().execute(() -> Unfloatable.get(level).remove(chunk, block));
+        if (((Trackable)event.newState().getBlock()).float$tracked()) level.getServer().execute(() -> Unfloatable.get(level).add(chunk, block));
     }
 
     @SuppressWarnings("PatternVariableCanBeUsed")
@@ -66,34 +64,38 @@ public final class GrassNotFloating {
             ChunkData<Long, BlockState> unfloatable = Unfloatable.get(level);
             Set<Long> tracked = unfloatable.viewChunk(chunkKey);
             if (tracked.isEmpty()) return;
-            BlockState air = AIR.get();
-            level.getServer().execute(() -> {
-                for (long pos : tracked) {
-                    BlockPos.MutableBlockPos potentialAirPos = new BlockPos.MutableBlockPos(BlockPos.getX(pos), BlockPos.getY(pos) - 1, BlockPos.getZ(pos));
-                    if (!level.isEmptyBlock(potentialAirPos)) continue;
+            level.getServer().execute(new AirDetect(tracked, level, chunkX, chunkZ, AIR.get(), chunkKey, unfloatable));
+        }
+    }
 
+    private record AirDetect(Set<Long> tracked, ServerLevel level, int chunkX, int chunkZ, BlockState air, long chunkKey, ChunkData<Long, BlockState> unfloatable) implements Runnable {
+        @Override
+        public void run() {
+            for (long pos : tracked) {
+                BlockPos.MutableBlockPos potentialAirPos = new BlockPos.MutableBlockPos(BlockPos.getX(pos), BlockPos.getY(pos) - 1, BlockPos.getZ(pos));
+                if (!level.isEmptyBlock(potentialAirPos)) continue;
+
+                potentialAirPos.setY(potentialAirPos.getY() + 1);
+
+                while (((Trackable) level.getBlockState(potentialAirPos).getBlock()).float$tracked()) {
+                    Executor.runAfter(1, new Removal(level, chunkX, chunkZ, potentialAirPos.immutable(), air));
                     potentialAirPos.setY(potentialAirPos.getY() + 1);
-
-                    while (((Trackable)level.getBlockState(potentialAirPos).getBlock()).float$tracked()) {
-                        BlockPos airPos = potentialAirPos.immutable();
-                        Runnable cleanTask = new Runnable() {
-                            @Override
-                            public void run() {
-                                if (level.hasChunk(chunkX, chunkZ)) {
-                                    level.setBlockAndUpdate(airPos, air);
-                                    LOGGER.info("Removing float block at {}", airPos);
-                                } else {
-                                    Executor.runAfter(1, this);
-                                }
-                            }
-                        };
-                        Executor.runAfter(1, cleanTask);
-                        potentialAirPos.setY(potentialAirPos.getY() + 1);
-                    }
                 }
+            }
 
-                unfloatable.data().remove(chunkKey);
-            });
+            unfloatable.data().remove(chunkKey);
+        }
+    }
+
+    private record Removal(ServerLevel level, int chunkX, int chunkZ, BlockPos airPos, BlockState air) implements Runnable {
+        @Override
+        public void run() {
+            if (level.hasChunk(chunkX, chunkZ)) {
+                LOGGER.info("Removing float {} at {}", level.getBlockState(airPos).getBlock().toString(), airPos);
+                level.setBlockAndUpdate(airPos, air);
+            } else {
+                Executor.runAfter(1, this);
+            }
         }
     }
 }
