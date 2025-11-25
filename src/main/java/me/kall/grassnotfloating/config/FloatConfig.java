9d67d80e9430a6b9584f50281d4bfd31ce5e402b
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-11-25 20:22:03 +0800

    tag support

--- src/main/java/me/kall/grassnotfloating/config/FloatConfig.java
+++ src/main/java/me/kall/grassnotfloating/config/FloatConfig.java
@@ -2,10 +2,10 @@ package me.kall.grassnotfloating.config;
 
 import com.google.common.base.Predicates;
 import com.google.common.collect.Lists;
-import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 import me.kall.grassnotfloating.GrassNotFloating;
 import me.kall.grassnotfloating.ext.Trackable;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
@@ -15,6 +15,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class FloatConfig {
     public static final ForgeConfigSpec INSTANCE;
@@ -23,15 +24,25 @@ public class FloatConfig {
     static {
         ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
         builder.push(GrassNotFloating.MOD_ID);
-        TRACKED = builder.defineList("UnfloatableBlocks", Lists.newArrayList("minecraft:grass", "minecraft:fern", "minecraft:tall_grass"), Predicates.alwaysTrue());
+        TRACKED = builder.comment("Support registry entries and tags", "Please add 'tag:' before tag or the parsing will go wrong", "'minecraft' namespace is not omittable").defineList("UnfloatableBlocks", Lists.newArrayList("minecraft:grass", "minecraft:fern", "minecraft:tall_grass", "tag:minecraft:flowers"), Predicates.alwaysTrue());
         builder.pop();
         INSTANCE = builder.build();
     }
 
     public static void initConfig() {
-        Set<String> tracked = new ObjectOpenHashSet<>(TRACKED.get());
+        Set<String> tracked = TRACKED.get().stream().filter(id -> !id.startsWith("tag:")).collect(Collectors.toSet());
+        Set<ResourceLocation> tags = TRACKED.get().stream().filter(id -> id.startsWith("tag:")).map(id -> {
+            try {
+                String[] parts = id.split(":");
+                return ResourceLocation.fromNamespaceAndPath(parts[1], parts[2]);
+            } catch (Exception exception) {
+                throw new RuntimeException("Invalid entry in GrassNotFloating config: " + id);
+            }
+        }).collect(Collectors.toSet());
         for (Map.Entry<ResourceKey<Block>, Block> entry : ForgeRegistries.BLOCKS.getEntries()) {
-            ((Trackable)entry.getValue()).float$setTracked(tracked.contains(entry.getKey().location().toString()));
+            String name = entry.getKey().location().toString();
+            Block block = entry.getValue();
+            ((Trackable)block).float$setTracked(tracked.contains(name) || block.defaultBlockState().getTags().anyMatch(blockTagKey -> tags.contains(blockTagKey.location())));
         }
     }
 
