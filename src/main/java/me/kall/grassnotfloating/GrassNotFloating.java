777ec50db52245cff83fe2fe95012512304ae5a7
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-11-28 22:18:13 +0800

    Update GrassNotFloating.java

--- src/main/java/me/kall/grassnotfloating/GrassNotFloating.java
+++ src/main/java/me/kall/grassnotfloating/GrassNotFloating.java
@@ -21,7 +21,6 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartedEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -52,7 +51,6 @@ public final class GrassNotFloating {
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, FloatConfig.INSTANCE);
     }
 
-    @SuppressWarnings("PatternVariableCanBeUsed")
     private void dataRebuild(WorldEvent.@NotNull Load event) {
         if (event.getWorld() instanceof ServerLevel) {
             ServerLevel level = (ServerLevel) event.getWorld();
